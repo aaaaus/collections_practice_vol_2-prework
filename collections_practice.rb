aad6fa66ced384aa679f3381afa69893bb53678a
Austin Luft <ausluft@gmail.com>
2018-09-26 17:19:59 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -65,7 +65,7 @@ end
 
 def merge_data(keys, data)
   
-# keys = [{:first_name=>"blake"}, {:first_name=>"ashley"}]
+keys = [{:first_name=>"blake"}, {:first_name=>"ashley"}]
 data = [{"blake"=>{:awesomeness=>10, :height=>"74", :last_name=>"johnson"},
 "ashley"=>{:awesomeness=>9, :height=>60, :last_name=>"dubs"}}]
 
